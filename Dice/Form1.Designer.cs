fa3068bcc504bee595e004dcc6e64b591b91c59b
Bikash Pokhrel <bpokhrel@aut.ac.nz>
2019-08-27 09:46:02 +1200

    The dice rool is done by adding a do while loop

--- Dice/Form1.Designer.cs
+++ Dice/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.lbxDiceRoll = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(562, 145);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(125, 72);
+            this.btnRoll.TabIndex = 0;
+            this.btnRoll.Text = "Roll the Dice";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.BtnRoll_Click);
+            // 
+            // lbxDiceRoll
+            // 
+            this.lbxDiceRoll.FormattingEnabled = true;
+            this.lbxDiceRoll.ItemHeight = 16;
+            this.lbxDiceRoll.Location = new System.Drawing.Point(85, 67);
+            this.lbxDiceRoll.Name = "lbxDiceRoll";
+            this.lbxDiceRoll.Size = new System.Drawing.Size(162, 324);
+            this.lbxDiceRoll.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbxDiceRoll);
+            this.Controls.Add(this.btnRoll);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.ListBox lbxDiceRoll;
     }
 }
 
--- Dice/Form1.cs
+++ Dice/Form1.cs
@@ -16,5 +16,21 @@ namespace Dice
         {
             InitializeComponent();
         }
+
+        private void BtnRoll_Click(object sender, EventArgs e)
+        {
+            // Define a random generator with millisecond as the seed
+            Random myrandom = new Random(DateTime.Now.Millisecond);
+            //define the variables that will set values with the initial value as 0
+            int Dice1, Dice2 = 0;
+            lbxDiceRoll.Items.Clear();
+            do //condition for rolling the dice
+            {
+                Dice1 = myrandom.Next(1, 7);
+                Dice2 = myrandom.Next(1, 7);
+            } while (Dice1 != Dice2);
+            //add the values of dice to the list box as a new item
+            lbxDiceRoll.Items.Add(Dice1 + " " + Dice2);
+        }
     }
 }
